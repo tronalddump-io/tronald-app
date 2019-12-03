79ae61f68b2d2c438e36f96d0cd1426709efa4a9
Mathias Schilling <m@matchilling.com>
2019-12-03 17:58:20 +0100

    Add quote HTML view

--- src/main/kotlin/io/tronalddump/app/quote/QuoteController.kt
+++ src/main/kotlin/io/tronalddump/app/quote/QuoteController.kt
@@ -6,6 +6,7 @@ import org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.ModelAndView
 
 @RequestMapping(value = [Url.QUOTE])
 @RestController
@@ -17,19 +18,25 @@ class QuoteController(
     @ResponseBody
     @RequestMapping(
             headers = [
+                "${HttpHeaders.ACCEPT}=$HAL_JSON_VALUE",
                 "${HttpHeaders.ACCEPT}=${MediaType.APPLICATION_JSON_VALUE}",
-                "${HttpHeaders.ACCEPT}=$HAL_JSON_VALUE"
+                "${HttpHeaders.ACCEPT}=${MediaType.TEXT_HTML_VALUE}"
             ],
             method = [RequestMethod.GET],
-            produces = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE],
             value = ["/{id}"]
     )
     fun findById(
             @RequestHeader(HttpHeaders.ACCEPT) acceptHeader: String,
             @PathVariable id: String
-    ): QuoteModel {
+    ): Any {
         val entity = repository.findById(id).orElseThrow {
             EntityNotFoundException("Quote with id \"$id\" not found.")
+        }
+
+        if (acceptHeader.contains(MediaType.TEXT_HTML_VALUE)) {
+            return ModelAndView("quote")
+                    .addObject("quote", entity)
         }
 
         return assembler.toModel(entity)
