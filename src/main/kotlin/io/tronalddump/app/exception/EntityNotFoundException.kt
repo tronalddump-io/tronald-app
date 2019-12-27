ccd9b6c5d50dde08d6dac88f107e91d92fe33b9f
Mathias Schilling <m@matchilling.com>
2019-12-27 10:00:34 +0100

    Return HTTP 404 on EntityNotFoundException

--- src/main/kotlin/io/tronalddump/app/exception/EntityNotFoundException.kt
+++ src/main/kotlin/io/tronalddump/app/exception/EntityNotFoundException.kt
@@ -1,3 +1,7 @@
 package io.tronalddump.app.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 class EntityNotFoundException(message: String) : RuntimeException(message)
